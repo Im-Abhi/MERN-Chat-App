f915caee40460989fd8549efa717c1d6854056d8
Im-Abhi <abhishekgupta942001@gmail.com>
2021-12-31 15:15:10 +0530

    Add Dummy data, create api end point

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,23 @@
 const express = require('express');
+const { chats } = require("./data/data");
 
 const app = express();
 const port = 4000;
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("API is running");
 });
 
-app.listen(()=>{
+app.get("/api/chat", (req, res) => {
+    res.send(chats);
+});
+
+app.get("/api/chat/:id", (req, res) => {
+    const chatId = req.params.id;
+    const singleChat = chats.find( chat => chat.id === chatId );
+    res.send(singleChat);
+});
+
+app.listen(() => {
     console.log(`Server started on port ${port}`);
 });
