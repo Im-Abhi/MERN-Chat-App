3b6dd4db856c2e8d035ef0458db827b8cd4bee79
Im-Abhi <abhishekgupta942001@gmail.com>
2022-01-04 17:56:03 +0530

    Add fields in signup component

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,29 +1,29 @@
 import React from "react";
 import { Box, Container, Text } from "@chakra-ui/react";
-import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react';
 import Login from "../components/authentication/Login";
 import SignUp from "../components/authentication/SignUp";
 
 const HomePage = () => {
     return (
-        <Container maxW='xl' centerContent>
+        <Container maxW='lg' centerContent>
             <Box
                 d='flex'
                 justifyContent='center'
                 p={3}
-                bg={"white"}
-                w={"100%"}
-                m={"40px 0 15px 0"}
-                borderRadius={"lg"}
-                borderWidth={"1px"}
+                bg="white"
+                w="100%"
+                m="20px 0 15px 0"
+                borderRadius="lg"
+                borderWidth="1px"
             >
-                <Text fontSize={"4xl"} fontFamily={"Work sans"} color={"black"}>
+                <Text fontSize="3xl" fontFamily="Work sans">
                     Chat-App
                 </Text>
             </Box>
-            <Box bg="white" w="100%" p={4} borderRadius={"lg"} borderWidth={"1px"}>
+            <Box bg="white" w="100%" p={3} borderRadius="lg" borderWidth="1px">
             <Tabs variant='soft-rounded'>
-                <TabList m="1em">
+                <TabList mb="1em">
                     <Tab width="50%">Login</Tab>
                     <Tab width="50%">Sign Up</Tab>
                 </TabList>
