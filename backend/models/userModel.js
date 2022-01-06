f1cdf19d7df502fe131cb1f9ec35a5df236aad4a
Im-Abhi <abhishekgupta942001@gmail.com>
2022-01-06 13:48:04 +0530

    Login api complete

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const { Schema, model } = mongoose;
+const bcrypt = require('bcrypt');
 
 const userSchema = Schema({
     name: { type: String, required: true },
@@ -13,6 +14,19 @@ const userSchema = Schema({
 {
     timestamps: true
 });
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+  
+userSchema.pre("save", async function (next) {
+    if (!this.isModified) {
+        next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});  
 
 const User = model("User", userSchema);
 
